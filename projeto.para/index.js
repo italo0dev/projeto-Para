675b72715662d0ac3f93a0b1f2500f071ee8103e
italo <italodev.uni11@gmail.com>
2025-05-01 09:48:06 -0300

    retirada de linha

--- projeto.para/index.js
+++ projeto.para/index.js
@@ -1,15 +1,13 @@
 console.log("ola mundo")
 document.getElementById('menu-button').addEventListener('click', function() {
     const menuList = document.getElementById('menu-list');
-    // Alterna a visibilidade da lista
     if (menuList.style.display === 'block') {
         menuList.style.display = 'none';
     } else {
         menuList.style.display = 'block';
     }
-});
+})
 
-// Fecha o menu se clicar fora dele
 window.addEventListener('click', function(event) {
     const menuList = document.getElementById('menu-list');
     const menuButton = document.getElementById('menu-button');
